1d14e22c69a217c0e1e1da8fb32742250d15f9a8
hefengh41-tech <hefengh41@gmail.com>
2026-04-20 18:13:48 +0300

    Create 12_7_model.py

---
+++ src/binn_full/12_7_model.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.utils import register_keras_serializable
+
+@register_keras_serializable()
+class BINN(tf.keras.Model):
+    def __init__(self, mask_cpg_gene, mask_gene_path):
+        super().__init__()
+
+        self.mask_cpg_gene = tf.constant(mask_cpg_gene, dtype=tf.float32)
+        self.mask_gene_path = tf.constant(mask_gene_path, dtype=tf.float32)
+
+        self.W1 = self.add_weight(shape=self.mask_cpg_gene.shape,
+                                 initializer="random_normal")
+
+        self.W2 = self.add_weight(shape=self.mask_gene_path.shape,
+                                 initializer="random_normal")
+
+        self.b1 = self.add_weight(shape=(self.mask_cpg_gene.shape[1],))
+        self.b2 = self.add_weight(shape=(self.mask_gene_path.shape[1],))
+
+        self.bn1 = layers.BatchNormalization()
+        self.bn2 = layers.BatchNormalization()
+        self.drop = layers.Dropout(0.3)
+
+    def call(self, x, training=False):
+        x = tf.matmul(x, self.W1 * self.mask_cpg_gene) + self.b1
+        x = tf.nn.relu(x)
+        x = self.bn1(x, training=training)
+        x = self.drop(x, training=training)
+
+        x = tf.matmul(x, self.W2 * self.mask_gene_path) + self.b2
+        x = self.bn2(x, training=training)
+
+        return tf.nn.tanh(x)
